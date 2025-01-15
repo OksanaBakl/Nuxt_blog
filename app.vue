9c93771876e591a9b98e3cd66018ae568234f831
oksanabaklan <oksana.molesha@gmail.com>
2025-01-15 22:00:29 +0100

    feat: initialize Nuxt project with Tailwind CSS

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-[#F1F2F4] dark:text-zinc-300 dark:bg-slate-950">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h2 class="text-2xl text-red-500">Welcome to My Blog!</h2>
+      <p class="text-lg">This is the homepage of my blog. Here, I will share interesting posts.</p>
+    </div>
+</template>
+
+<script setup></script>
+  
